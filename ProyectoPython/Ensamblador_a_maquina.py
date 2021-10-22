678d230ef79fd2ca8d905631e52b46200b26072a
Gustavo Solorio <gustavo.solorio@iteso.mx>
2021-10-22 02:11:14 -0500

    Sustitución de la 2a columna de resultado

--- ProyectoPython/Ensamblador_a_maquina.py
+++ ProyectoPython/Ensamblador_a_maquina.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # vi: set shiftwidth=4 tabstop=8 expandtab:
 #
+resultado = []
 matriz = []
 auxiliar = []
 valor_etiqueta= []
 auxiliar2 = []
 imm = []
+relleno = "00000"
+rellenojr = "00000000000"
 ######## Definicion de diccionarios ############
 etiquetas= {}
 
 oppcode = {'add':'0000','addi':'0001','and':'0010','andi':'0011','beq':'0100',\
         'bne':'0101','j':'0110', 'jal':'0111','jr':'1010','lb':'1011','or':'1100',\
         'sb':'1101','sll':'1110','srl':'1111' }
-Rx ={'x0':'000','x1':'001','x2':'010','x3':'011','x4':'100','x5':'101','x6':'110',\
+Rx={'x0':'000','x1':'001','x2':'010','x3':'011','x4':'100','x5':'101','x6':'110',\
 'x7':'111','X0':'000','X1':'001','X2':'010','X3':'011','X4':'100','X5':'101',\
 'X6':'110','X7':'111'}
 ###########################################
@@ -56,10 +59,9 @@ Diccionario etiquetas
 
 for j in range (len(matriz_datos)):
     matriz.append([])
+    resultado.append([])
     matriz[j].extend(auxiliar[j].split(","))
-
-#for j in range(len(matriz_datos)):
-#    print(matriz[j])
+    resultado[j].extend(auxiliar[j].split(","))
 
 """
 """
@@ -83,8 +85,11 @@ for l in range (len(imm)):
         imm[l] = (bin(((1 << 8) -1) & imm[l])[2:]).zfill(8)
 #imprime los valores en 8 bits de imm
 print(imm)
-"""
-"""
+print(etiquetas)
+
+print("**matriz***")
+for j in range (len(matriz_datos)):
+    print(matriz[j])
 
 """          i
     matriz= [ ,  ,  , ] j
@@ -92,20 +97,41 @@ print(imm)
             [ ,  ,  , ]
 """
 
-"""
+
 for j in range (len(matriz_datos)):
     for i in range(len(matriz[j])):
         if i == 0:
-            matriz[j][i]=oppcode[matriz[j][i]]
-       # elif i == 1:
+            resultado[j][i] = oppcode[matriz[j][i]]
+        elif i==1:
+            if (matriz[j][0] == "j")or(matriz[j][0] == "jal"):
+                if matriz[j][i] in etiquetas.keys():
+                    tag = etiquetas[matriz[j][i]]
+                else:
+                    tag = eval(matriz[j][i])
+                resultado[j][i] = (bin(((1<<14)-1)& tag)[2:]).zfill(14)
+            elif (matriz[j][0] == "jr"):
+                resultado[j][i] = Rx[matriz[j][i]] + rellenojr
+            elif (matriz[j][0] == "bne") or (matriz[j][0] == "beq"):
+                resultado[j][i] = Rx[matriz[j][i]]
+            elif (matriz[j][0] == "sll"):
+                resultado[j][i] = Rx[matriz[j][i]]
+            elif (matriz[j][0] == "slr"):
+                resultado[j][i] = Rx[matriz[j][i+1]]
+            elif (matriz[j][0] == "sb") or (matriz[j][0] == "lb"):
+                resultado[j][i] = Rx[matriz[j][i+2]]
+            else:
+                resultado[j][i] = Rx[matriz[j][i+1]]
        #     matriz[j][i]=Rx[matriz[j][i]]
        # elif i== 2:
        #     matriz[j][i]=Rx[matriz[j][i]]
 
-print ( "\n*********\n")
+print("\n****resultado*****\n")
+for j in range(len(matriz_datos)):
+    print(resultado[j])
+
+print("****matriz***")
 for j in range(len(matriz_datos)):
     print(matriz[j])
-"""
 archivo.close()
 #archivo_salida= open("./salida.txt","w")
 #archivo_salida.write(salida)
